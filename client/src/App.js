8c7222a76470874c291c8420586281662579a1b8
LauraE2020 <smalltowngirl591@gmail.com>
2020-06-05 14:36:07 -0400

    implemented a response nav menu & nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
@@ -8,14 +9,54 @@ import Events from './components/Events';
 import Contact from './components/Contact';
 
 
+
+
 function App() {
+
+const [navMenuClasses, setNavMenuClasses] = useState("nav-menu");
+
+  //if (show) {
+  //  navMenuClass = "nav-menu open";
+  //}
+
+  const navMenuOpen = () => {
+    setNavMenuClasses("nav-menu open")
+  }
+
+  const navMenuClose = () => {
+    setNavMenuClasses("nav-menu")
+  }
+
   return (
+
     <div className="App">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/shopping">Shopping</Link>
-      <Link to="/events">Events</Link>
-      <Link to="/contact">Contact</Link>
+      <div className="nav-container">
+        <nav className="nav-bar">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/shopping">Shopping</Link></li>
+          <li><Link to="/events">Events</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <Link to="/shopping/cart" className="primary-btn">Cart</Link>
+        </nav>
+
+        <nav className={navMenuClasses}>
+        <span
+        onClick={navMenuClose} className="close-btn"><i className="fas fa-times"></i></span>
+          <li><Link to="/" onClick={navMenuClose}>Home</Link></li>
+          <li><Link to="/about" onClick={navMenuClose}>About</Link></li>
+          <li><Link to="/shopping" onClick={navMenuClose}>Shopping</Link></li>
+          <li><Link to="/events" onClick={navMenuClose}>Events</Link></li>
+          <li><Link to="/contact" onClick={navMenuClose}>Contact</Link></li>
+
+        </nav>
+        <div
+        onClick={navMenuOpen} className="burger-menu-icon">
+          <i className="fas fa-bars"></i>
+        </div>
+      </div>
+
+
 
       <Switch>
         <Route exact path="/"><Home /></Route>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link, useLocation, useParams, useHistory, useRouteMatch } from 'react-router-dom';
+import './css/home.css';
+
+
 
 import '../App.css';
 
 function Home() {
   return (
-    <div className="App">
-      <h1>Home</h1>
+    <div className="Home">
+      <header>
+        <div className="hero-img">Hi</div>
+        <h1 className="home-header">Old Pioneer Store & Emporium</h1>
+      </header>
     </div>
   );
 }

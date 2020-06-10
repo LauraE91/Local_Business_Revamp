08ab1b6b461e83e722d0d7c6a2691a874a9c279d
LauraE2020 <smalltowngirl591@gmail.com>
2020-06-10 19:14:58 -0400

    Added alt tags to images

--- client/src/components/Home/HomeShoppingSection.js
+++ client/src/components/Home/HomeShoppingSection.js
@@ -9,24 +9,24 @@ function HomeShoppingSection() {
        <h1 className="title-2 light-clr">Shopping</h1>
       <div className="global-card-container">
         <ShoppingCard>
-          <img className="card-img" src="/img/mi.jpg"/>
+          <img className="card-img" src="/img/mi.jpg" alt="michigan ornament"/>
           <h4 className="dark-clr title-4">Michigan Products</h4>
           <button className="primary-btn">View</button>
         </ShoppingCard>
 
         <ShoppingCard>
-          <img className="card-img" src="/img/vase.jpg"/>
+          <img className="card-img" src="/img/vase.jpg" alt="Fenton Art Glass vase"/>
           <h4 className="dark-clr title-4">Fenton Art Glass</h4>
           <button className="primary-btn">View</button>
         </ShoppingCard>
 
         <ShoppingCard>
-          <img className="card-img" src="/img/kilwins_coffee.jpg"/>
+          <img className="card-img" src="/img/kilwins_coffee.jpg"alt="Kilwin's Coffee"/>
           <h4 className="dark-clr title-4">Kilwin's Coffee</h4>
           <button className="primary-btn">View</button>
         </ShoppingCard>
 
-      </div>
+      </div>S
 
     </section>
   )
--- client/src/components/Shopping.js
+++ client/src/components/Shopping.js
@@ -10,37 +10,37 @@ function Shopping() {
       <h1 className="title-1">Our Inventory</h1>
       <section className="products-container">
         <ShoppingCard>
-          <img className="card-img" src="/img/decor2.jpg"/>
+          <img className="card-img" src="/img/decor2.jpg" alt="'Every Moment Matters' sign"/>
           <h4 className="title-4">"Every Moment Matters" Sign</h4>
           <button className="primary-btn">View</button>
         </ShoppingCard>
 
         <ShoppingCard>
-          <img className="card-img" src="/img/decor3.jpg"/>
+          <img className="card-img" src="/img/decor3.jpg" alt="'Welcome to the Chaos' sign"/>
           <h4 className="title-4">"Welcome to the Chaos" Sign</h4>
           <button className="primary-btn">View</button>
         </ShoppingCard>
 
         <ShoppingCard>
-          <img className="card-img" src="/img/vanilla-lotion.jpg"/>
+          <img className="card-img" src="/img/vanilla-lotion.jpg" alt="Vanilla Lotion"/>
           <h4 className="title-4">Vanilla Lotion</h4>
           <button className="primary-btn">View</button>
         </ShoppingCard>
 
         <ShoppingCard>
-          <img className="card-img" src="/img/lavender-lotion.jpg"/>
+          <img className="card-img" src="/img/lavender-lotion.jpg" alt="Lavender Lotion"/>
           <h4 className="title-4">Lavender Lotion</h4>
           <button className="primary-btn">View</button>
         </ShoppingCard>
 
         <ShoppingCard>
-          <img className="card-img" src="/img/puzzle1.jpg"/>
+          <img className="card-img" src="/img/puzzle1.jpg" alt="Lake Supperior Agates Puzzle"/>
           <h4 className="title-4">Lake Supperior Agates Puzzle</h4>
           <button className="primary-btn">View</button>
         </ShoppingCard>
 
         <ShoppingCard>
-          <img className="card-img" src="/img/puzzle2.jpg"/>
+          <img className="card-img" src="/img/puzzle2.jpg" alt="Grand Hotel Puzzle"/>
           <h4 className="title-4">Grand Hotel Puzzle</h4>
           <button className="primary-btn">View</button>
         </ShoppingCard>
